2837350667f29fd27b74dc50f0692b378cbf1707
Marin Todorov <marin@underplot.com>
2016-12-07 16:06:36 +0100

    initial commit

--- Example/RxRealmDataSources/ViewController.swift
+++ Example/RxRealmDataSources/ViewController.swift
@@ -30,25 +30,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // load laps from Realm
         var config = Realm.Configuration.defaultConfiguration
         config.inMemoryIdentifier = "Memory"
 
+        let realm = try! Realm(configuration: config)
+        let results = realm.objects(Lap.self)
+
+
+        // create data source
         let dataSource = RxTableViewRealmDataSource<Lap>(cellIdentifier: "Cell") {ds, tv, ip, lap in
             let cell = tv.dequeueReusableCell(withIdentifier: ds.cellIdentifier, for: ip) as! PersonCell
             cell.customLabel.text = "\(ip.row). \(lap.text)"
             return cell
         }
 
-        let realm = try! Realm(configuration: config)
-        let results = realm.objects(Lap.self)
-
+        // RxRealm to get Observable<Results>
         let laps = Observable.changesetFrom(results)
             .share()
 
+        // bind to table view
         laps
             .bindTo(tableView.rx.realmChanges(dataSource))
             .addDisposableTo(bag)
 
+        // bind to vc title
         laps
             .map { results, _ in
                 return "\(results.count) laps"
@@ -56,6 +62,7 @@ class ViewController: UIViewController {
             .bindTo(rx.title)
             .addDisposableTo(bag)
 
+        // insert some laps
         for i in 0...200 {
             delay(seconds: Double(i)/2, completion: {
                 let realm = try! Realm(configuration: config)
