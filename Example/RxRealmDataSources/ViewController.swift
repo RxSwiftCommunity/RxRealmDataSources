b40fe8c5bf5c0d4467792f1b84f9f458d1976170
Marin Todorov <marin@underplot.com>
2016-12-07 15:56:05 +0100

    initial commit

---
+++ Example/RxRealmDataSources/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  RxRealmDataSources
+//
+//  Created by Marin Todorov on 12/07/2016.
+//  Copyright (c) 2016 Marin Todorov. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import RealmSwift
+import RxRealm
+import RxRealmDataSources
+
+class Lap: Object {
+    dynamic var text = Date().description
+}
+
+func delay(seconds: Double, completion: @escaping ()->Void) {
+    DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: completion)
+}
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    let bag = DisposeBag()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        var config = Realm.Configuration.defaultConfiguration
+        config.inMemoryIdentifier = "Memory"
+
+        let dataSource = RxTableViewRealmDataSource<Lap>(cellIdentifier: "Cell") {ds, tv, ip, lap in
+            let cell = tv.dequeueReusableCell(withIdentifier: ds.cellIdentifier, for: ip) as! PersonCell
+            cell.customLabel.text = "\(ip.row). \(lap.text)"
+            return cell
+        }
+
+        let realm = try! Realm(configuration: config)
+        let results = realm.objects(Lap.self)
+
+        let laps = Observable.changesetFrom(results)
+            .share()
+
+        laps
+            .bindTo(tableView.rx.realmChanges(dataSource))
+            .addDisposableTo(bag)
+
+        laps
+            .map { results, _ in
+                return "\(results.count) laps"
+            }
+            .bindTo(rx.title)
+            .addDisposableTo(bag)
+
+        for i in 0...200 {
+            delay(seconds: Double(i)/2, completion: {
+                let realm = try! Realm(configuration: config)
+                try! realm.write {
+                    realm.add(Lap())
+                }
+            })
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # RxRealmDataSources
-An easy way to bind an RxRealm observable to a table or collection view 
+
+[![CI Status](http://img.shields.io/travis/Marin Todorov/RxRealmDataSources.svg?style=flat)](https://travis-ci.org/Marin Todorov/RxRealmDataSources)
+[![Version](https://img.shields.io/cocoapods/v/RxRealmDataSources.svg?style=flat)](http://cocoapods.org/pods/RxRealmDataSources)
+[![License](https://img.shields.io/cocoapods/l/RxRealmDataSources.svg?style=flat)](http://cocoapods.org/pods/RxRealmDataSources)
+[![Platform](https://img.shields.io/cocoapods/p/RxRealmDataSources.svg?style=flat)](http://cocoapods.org/pods/RxRealmDataSources)
+
+## Example
+
+To run the example project, clone the repo, and run `pod install` from the Example directory first.
+
+## Requirements
+
+## Installation
+
+RxRealmDataSources is available through [CocoaPods](http://cocoapods.org). To install
+it, simply add the following line to your Podfile:
+
+```ruby
+pod "RxRealmDataSources"
+```
+
+## Author
+
+Marin Todorov, marin@underplot.com
+
+## License
+
+RxRealmDataSources is available under the MIT license. See the LICENSE file for more info.
