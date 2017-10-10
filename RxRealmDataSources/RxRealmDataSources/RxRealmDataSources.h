c8ed4e51ffd33158701eb2f47cd45341f0a9f2c3
Rob Cheung <rob@finxpc.com>
2017-10-09 19:10:01 -0700

    set up project for carthage

---
+++ RxRealmDataSources/RxRealmDataSources/RxRealmDataSources.h
@@ -0,0 +1,19 @@
+//
+//  RxRealmDataSources_iOS.h
+//  RxRealmDataSources_iOS
+//
+//  Created by Rob Cheung on 10/9/17.
+//  Copyright © 2017 RxSwiftCommunity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxRealmDataSources_iOS.
+FOUNDATION_EXPORT double RxRealmDataSources_iOSVersionNumber;
+
+//! Project version string for RxRealmDataSources_iOS.
+FOUNDATION_EXPORT const unsigned char RxRealmDataSources_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxRealmDataSources_iOS/PublicHeader.h>
+
+
