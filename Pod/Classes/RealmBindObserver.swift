0dcd772c03ea0a5a12db715b85364103373c3b70
Rob Cheung <rob@finxpc.com>
2017-10-10 19:59:26 -0700

    4 spaces is crazy

--- Pod/Classes/RealmBindObserver.swift
+++ Pod/Classes/RealmBindObserver.swift
@@ -12,30 +12,30 @@ import RxSwift
 import RxCocoa
 import RxRealm
 
-public class RealmBindObserver<O:Object, C:RealmCollection, DS>: ObserverType {
-  typealias BindingType = (DS, C, RealmChangeset?) -> Void
-  public typealias E = (C, RealmChangeset?)
+public class RealmBindObserver<O: Object, C: RealmCollection, DS>: ObserverType {
+    typealias BindingType = (DS, C, RealmChangeset?) -> Void
+    public typealias E = (C, RealmChangeset?)
 
-  let dataSource: DS
-  let binding: BindingType
+    let dataSource: DS
+    let binding: BindingType
 
-  init(dataSource: DS, binding: @escaping BindingType) {
-    self.dataSource = dataSource
-    self.binding = binding
-  }
-
-  public func on(_ event: Event<E>) {
-    switch event {
-    case .next(let element):
-      binding(dataSource, element.0, element.1)
-    case .error:
-      return
-    case .completed:
-      return
+    init(dataSource: DS, binding: @escaping BindingType) {
+        self.dataSource = dataSource
+        self.binding = binding
     }
-  }
 
-  func asObserver() -> AnyObserver<E> {
-    return AnyObserver(eventHandler: on)
-  }
+    public func on(_ event: Event<E>) {
+        switch event {
+        case .next(let element):
+            binding(dataSource, element.0, element.1)
+        case .error:
+            return
+        case .completed:
+            return
+        }
+    }
+
+    func asObserver() -> AnyObserver<E> {
+        return AnyObserver(eventHandler: on)
+    }
 }
