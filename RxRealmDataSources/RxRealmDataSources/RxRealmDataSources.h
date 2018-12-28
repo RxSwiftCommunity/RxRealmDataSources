89af35156e817022ca1c7de5ac63237abe3676db
Loki <sergiy@vynnychenko.com>
2018-12-28 22:24:01 +0200

    framework project

---
+++ RxRealmDataSources/RxRealmDataSources/RxRealmDataSources.h
@@ -0,0 +1,19 @@
+//
+//  RxRealmDataSources.h
+//  RxRealmDataSources
+//
+//  Created by Loki on 12/28/18.
+//  Copyright © 2018 Rx. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RxRealmDataSources.
+FOUNDATION_EXPORT double RxRealmDataSourcesVersionNumber;
+
+//! Project version string for RxRealmDataSources.
+FOUNDATION_EXPORT const unsigned char RxRealmDataSourcesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxRealmDataSources/PublicHeader.h>
+
+
